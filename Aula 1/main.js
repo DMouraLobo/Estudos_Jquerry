193f88ac31d1875f49b86acb48aa92bc6d9666ce
DMouraLobo <diegomouralobo@outlook.com>
2026-05-26 21:45:30 -0300

    Aula 2
    Aprendendo a manipular eventos com Jquerry

--- Aula 1/main.js
+++ Aula 1/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
-    console.log(document.querySelector('header button'))
-    console.log($('#botao-cancelar'))
+    $('header button').click(function(){
+        alert("Expandir formulário")
+    })
+    
+    $('form').on('submit', function(e) {
+        console.log("submit")
+        e.preventDefault();
+
+    })
+    
 })
