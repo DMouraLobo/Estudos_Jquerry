a0f537d2d63c234a0badd222578e2ad1d7b51bb3
DMouraLobo <diegomouralobo@outlook.com>
2026-05-31 17:56:38 -0300

    Aula 4
    Aprendendo a manipular DOM com JQUERY

--- Aula 1/main.js
+++ Aula 1/main.js
@@ -8,9 +8,19 @@ $(document).ready(function(){
     })
     
     $('form').on('submit', function(e) {
-        console.log("submit")
         e.preventDefault();
-
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src ="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class ="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" title="Ver imagen em tamanho real" target="_blank"
+                    Ver Imagem em tamanho real
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(5000);
+        $('#endereco-imagem-nova').val('')
     })
     
 })
